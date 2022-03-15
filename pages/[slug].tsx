40c6ddfacbddf99de24690d4131c98b6ad3d4074
Amir Ardalan <amirrardalan@gmail.com>
2022-03-15 05:12:57 -0700

    Add headers to fetch requests

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -6,9 +6,10 @@ import { title } from '@/data/content'
 
 
 const URL = process.env.NEXT_PUBLIC_SITE_URL
+const options = { method: "GET", headers: { Accept: "application/json; charset=UTF-8" }}
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const res = await fetch(`${URL}/api/nft-assets`)
+  const res = await fetch(`${URL}/api/nft-assets`, options)
   const assets = await res.json()
   const paths = assets.nfts.map((asset: any) => ({ 
     params: { slug: asset.slug},
@@ -17,7 +18,11 @@ export const getStaticPaths: GetStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const res = await fetch(`${URL}/api/nft-assets`)
+  const res = await fetch(`${URL}/api/nft-assets`, {
+    method: "GET",
+    headers: { Accept: "application/json; charset=UTF-8"},
+  }
+)
   const assets = await res.json()
   const assetIndex = assets.nfts.findIndex((
     x: { slug: Array<string> }) => x.slug === params.slug
