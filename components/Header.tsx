4173d2515479703963c0b7a2f4b55ee070935e82
Amir Ardalan <amirrardalan@gmail.com>
2022-03-28 03:58:52 -0700

    Fix header controls cotnainer

--- components/Header.tsx
+++ components/Header.tsx
@@ -40,7 +40,7 @@ const Header = ({ toggleTheme }) => {
     justifyContent: 'space-between',
     a:  { textDecoration: 'none' },
     '.headerRight': {
-      minWidth: '395.2px',
+      minWidth: 386.4,
       display: 'flex',
       flexDirection: 'row',
       alignItems: 'center',
